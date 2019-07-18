b8d21f4b524fdf9e6fca110b6b6f613ead12026d
BrandtM <marius.brandt@hotmail.com>
2019-07-18 20:03:11 +0200

    Improve documentation; make executable runnable from cargo-example

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
-# cargo-example
+`cargo-example` is a easy-to-use example runner for cargo projects.
 
-What if you didn't have to manually check-out a repository just to run its examples? This project is supposed to make running examples much easier.  
+# About
 
-## Early WIP warning
+`cargo-example` aims to provide a simple interface to running examples
+of any valid rust crate. Currently there is no short way to do this.
+In order to run examples you'll always have to manually clone or download
+the repository of the crate whose examples you wish to run.
+For newcomers to the rust language the `example` feature of cargo
+might even remain completely unknown for quite some time.  
+This crate aims to simplify this process. All the messy cloning
+is abstracted away from the end user.
 
-This repository is still a *very* early work-in-progress. It might clutter your hard drive or refuse to run altogether.
+# Usage
+
+## As a user
+
+TBD. Test if cargo install works!
+
+## In development
+
+Just use `cargo run example ...args` where args are the arguments you wish
+to give to the `cargo --example` command.
+
+# Inspiration
+
+I wouldn't have made this project if it wasn't for the excellent GitHub
+Project "request-for-implementation" by dtolnay. Specifically
+[this](https://github.com/dtolnay/request-for-implementation/issues/30)
+issue. 
+
+# Early stages
+
+This entire project is still in its infancy. It might not work for you.
+But you can help. Just open up a new GitHub issue if you find an error
+or would like to request a feature or a change.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,47 @@
+//! `cargo-example` is a easy-to-use example runner for cargo projects.
+//! 
+//! ## About
+//! 
+//! `cargo-example` aims to provide a simple interface to running examples
+//! of any valid rust crate. Currently there is no short way to do this.
+//! In order to run examples you'll always have to manually clone or download
+//! the repository of the crate whose examples you wish to run.
+//! For newcomers to the rust language the `example` feature of cargo
+//! might even remain completely unknown for quite some time.  
+//! This crate aims to simplify this process. All the messy cloning
+//! is abstracted away from the end user.
+//! 
+//! ## Usage
+//! 
+//! ### As a user
+//! 
+//! TBD. Test if cargo install works!
+//! 
+//! ### In development
+//! 
+//! Just use `cargo run example ...args` where args are the arguments you wish
+//! to give to the `cargo --example` command.
+//! 
+//! ## Inspiration
+//! 
+//! I wouldn't have made this project if it wasn't for the excellent GitHub
+//! Project "request-for-implementation" by dtolnay. Specifically
+//! [this](https://github.com/dtolnay/request-for-implementation/issues/30)
+//! issue. 
+//! 
+//! ## Early stages
+//! 
+//! This entire project is still in its infancy. It might not work for you.
+//! But you can help. Just open up a new GitHub issue if you find an error
+//! or would like to request a feature or a change.
+
 mod cache;
 mod download;
 mod error;
 mod response;
 
 use cache::*;
-use clap::{App, Arg, Values};
+use clap::{App, AppSettings, Arg, Values, SubCommand};
 use download::*;
 use std::process::{Command, Stdio};
 
@@ -22,13 +59,18 @@ fn run_command(args: &Vec<&str>, path: &str) {
 fn main() {
     let matches = App::new("cargo-example")
         .version("0.1")
-        .about("An easy example runner for cargo")
-        .arg(Arg::with_name("project").required(true).index(1))
-        .arg(Arg::with_name("additional_args").multiple(true))
+        .bin_name("cargo")
+        .settings(&[AppSettings::SubcommandRequired])
+        .subcommand(SubCommand::with_name("example")
+            .about("An easy example runner for cargo")
+            .arg(Arg::with_name("project").required(true).index(1))
+            .arg(Arg::with_name("additional_args").multiple(true))
+        )
         .get_matches();
-
-    let project = matches.value_of("project").unwrap();
-    let example_args: Vec<&str> = matches
+    
+    let subcommand_matches = matches.subcommand_matches("example").unwrap();
+    let project = subcommand_matches.value_of("project").unwrap();
+    let example_args: Vec<&str> = subcommand_matches
         .values_of("additional_args")
         .unwrap_or(Values::default())
         .collect();
